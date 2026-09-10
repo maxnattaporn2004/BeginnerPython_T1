934cbf6171e0d653677c180fe1323676df54dd5f
maxnattaporn2004 <maxnattaporn2004@gmail.com>
2026-09-10 15:11:12 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+English = 60.5
+Business = 66.5
+Computer = 99.9
+Programming = 100.0
+print("--- Your Score ---")
+print("Foundation English :" ,  English)
+print("General Business :" , Business)
+print("Interodeuction to Computer Systems :" , Computer)
+print("Computer Programming :" , Programming)
