d7709ea0744871e3c9c7d522d46b86b179f5acaa
maxnattaporn2004 <maxnattaporn2004@gmail.com>
2026-09-22 23:02:22 +0700

    Update list to dict

--- ListExam.py
+++ ListExam.py
@@ -14,17 +14,23 @@ def vatShowBill(total):
     vat = total+(total*0.07)
     print("ราคารวม = %f Bath" %vat)
 
+systemMenu = {
+                "ข้าวมันไก่":50,"ข้าวผัด":50,"ข้าว":10,"เส้น":15,
+                "1":"ข้าวมันไก่","2":"ข้าวผัด","3":"ข้าว","4":"เส้น"
+              }
 menuList =[]
-
-
+print("ข้าวมันไก่  = 1")
+print("ข้าวผัด    = 2")
+print("ข้าว      = 3")
+print("เส้น      = 4")
 while True:
     meunName =input("Please enter menu").capitalize()
     if meunName == "Exit":
         print("Thank You")
         break
     else:
-        menuPrice = int(input("Please enter price"))
-        menuList.append([meunName,menuPrice])
+        menuList.append([systemMenu[meunName],systemMenu[systemMenu[meunName]]])
+        print(menuList)
 
 
 showBill()
